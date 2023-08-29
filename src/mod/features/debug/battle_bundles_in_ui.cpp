d1604f458b63414709f511667e05f77d00e5b2fe
SaltContainer <fanmc34@gmail.com>
2023-08-29 16:58:17 -0400

    Testing combo pokemon bundles again

--- src/mod/features/debug/battle_bundles_in_ui.cpp
+++ src/mod/features/debug/battle_bundles_in_ui.cpp
@@ -1,7 +1,10 @@
 #include "exlaunch.hpp"
 
+#include "externals/BattlePokemonEntity.h"
 #include "externals/FlagWork.h"
 #include "externals/System/String.h"
+#include "externals/UnityEngine/GameObject.h"
+#include "ui/ui.h"
 
 #include "logger/logger.h"
 
@@ -36,22 +39,42 @@ HOOK_DEFINE_REPLACE(ReplacePathMethod) {
     }
 };
 
+HOOK_DEFINE_TRAMPOLINE(TestingAnimBattle) {
+    static void Callback(void* __this, int32_t index, float duration, float startTime) {
+        Logger::log("Call to AnimationPlayer$$Play(%d, %f, %f)\n", index, duration, startTime);
+        Orig(__this, index, duration, startTime);
+    }
+};
+
+HOOK_DEFINE_INLINE(TestingTransformBattle) {
+    static void Callback(exl::hook::nx64::InlineCtx* ctx) {
+        auto gameObject = (UnityEngine::GameObject::Object*)ctx->X[0];
+
+        Logger::log("TestingTransformBattle\n");
+
+        auto transform = gameObject->get_transform();
+        ui_inspect(transform);
+
+        ctx->X[0] = (uint64_t)transform;
+    }
+};
+
 void exl_battle_bundles_in_ui_main() {
     //// StringLiteral_9287 "pokemons/field/"
     // Dpr.UI.UIManager$$GetPokemonAssetbundleName
     ReplaceFullPath::InstallAtOffset(0x017c2014);
 
     // Dpr.SubContents.Utils$$GetPokemonAssetbundleName
-    //ReplacePathMethod::InstallAtOffset(0x0186b360);
+    ReplacePathMethod::InstallAtOffset(0x0186b360);
 
     // Dpr.EvScript.EvDataManager.<PreRequestAssetSetUp>d__1779$$MoveNext
-    //ReplacePathPrefix::InstallAtOffset(0x019b0130);
+    ReplacePathPrefix::InstallAtOffset(0x019b0130);
 
     // Dpr.EvScript.EvDataManager.<RequestAssetSetUp>d__1778$$MoveNext
-    //ReplacePathPrefix::InstallAtOffset(0x019b0f0c);
+    ReplacePathPrefix::InstallAtOffset(0x019b0f0c);
 
     // UgResManager$$AppendAsset
-    //ReplaceFullPath::InstallAtOffset(0x01b1b27c);
+    ReplaceFullPath::InstallAtOffset(0x01b1b27c);
 
 
     //// StringLiteral_18434 "pokemons/field/pm{0:D4}_00_00"
@@ -61,9 +84,20 @@ void exl_battle_bundles_in_ui_main() {
 
     //// StringLiteral_10105 "pokemons/field/{0}"
     // Dpr.SecretBase.PlacementData$$Destroy
-    //ReplaceFullPath::InstallAtOffset(0x019c9420);
+    ReplaceFullPath::InstallAtOffset(0x019c9420);
 
     // LightStoneResourcesManager.<DataLoad>d__31$$MoveNext
-    //ReplaceFullPath::InstallAtOffset(0x01af27c8);
-    //ReplaceFullPath::InstallAtOffset(0x01af27fc);
+    ReplaceFullPath::InstallAtOffset(0x01af27c8);
+    ReplaceFullPath::InstallAtOffset(0x01af27fc);
+
+
+    //TestingAnimBattle::InstallAtOffset(0x0211e9b0);
+    TestingTransformBattle::InstallAtOffset(0x01a122f0);
+
+    /*using namespace exl::armv8::inst;
+    using namespace exl::armv8::reg;
+    exl::patch::CodePatcher p(0x0173abb0);
+    p.WriteInst(Nop());
+    p.Seek(0x0173ad20);
+    p.WriteInst(Nop());*/
 }
