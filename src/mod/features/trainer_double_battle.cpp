89ecdb4c2e1dcb3cf30c7d10a653e82a81b896d9
Huevos <39507107+El-Huevo@users.noreply.github.com>
2024-03-09 18:49:50 +1000

    Flagwork Changes

--- src/mod/features/trainer_double_battle.cpp
+++ src/mod/features/trainer_double_battle.cpp
@@ -3,18 +3,21 @@
 #include "externals/il2cpp-api.h"
 #include "externals/Dpr/Battle/Logic/BATTLE_SETUP_PARAM.h"
 #include "externals/PlayerWork.h"
+#include "externals/Dpr/Battle/Logic/BtlRule.h"
+#include "externals/FlagWork.h"
+#include "externals/FlagWork_Enums.h"
 
-const int32_t BTL_RULE_SINGLE = 0;
-const int32_t BTL_RULE_DOUBLE = 1;
+
+using namespace Dpr::Battle::Logic;
 
 HOOK_DEFINE_TRAMPOLINE(SetupBattleTrainer) {
-    static void Callback(Dpr::Battle::Logic::BATTLE_SETUP_PARAM::Object *battleSetupParam, int32_t arenaID,
+    static void Callback(BATTLE_SETUP_PARAM::Object *battleSetupParam, int32_t arenaID,
                          int32_t mapAttrib, int32_t weatherType, int32_t rule, int32_t enemyID0, int32_t enemyID1,
                          int32_t partnerID, MethodInfo *method)
     {
-        if (PlayerWork::GetBool(2196))
+        if (FlagWork::GetFlag(FlagWork_Flag::FLAG_TRAINER_DOUBLE))
         {
-            rule = BTL_RULE_DOUBLE;
+            rule = (int32_t) BtlRule::BTL_RULE_DOUBLE;
         }
         Orig(battleSetupParam, arenaID, mapAttrib, weatherType, rule, enemyID0, enemyID1, partnerID, method);
     }
