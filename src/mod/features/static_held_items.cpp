7067f444e82a11d7d8ef7d63959f6dd605368006
Huevos <39507107+El-Huevo@users.noreply.github.com>
2024-03-11 10:40:45 +1000

    Simplify Static Held Items
    - Removed redundant code already covered by wild_held_items.cpp
    - Added egg check to wild_held_items.cpp

--- src/mod/features/static_held_items.cpp
+++ src/mod/features/static_held_items.cpp
@@ -20,33 +20,7 @@ using namespace Pml;
 HOOK_DEFINE_INLINE(SetWildPokemonItem) {
     static void Callback(exl::hook::nx64::InlineCtx* ctx) {
 
-        const int32_t ITEM1_BASERATE = 50;
-        const int32_t ITEM1_BOOSTEDRATE = 60;
-        const int32_t ITEM2_BASERATE = 20;
-        const int32_t ITEM2_BOOSTEDRATE = 20;
-
-        int32_t item1Rate;
-        int32_t item2Rate;
-
         system_load_typeinfo(0x3f8c);
-
-        PokeParty::Object *party = PlayerWork::get_playerParty();
-        auto firstPoke = (PokePara::CoreParam *) party->GetMemberPointer(0);
-
-        if (!firstPoke->IsEgg(PokePara::EggCheckType::BOTH_EGG)) {
-            int32_t ability = firstPoke->GetTokuseiNo();
-            switch (ability) {
-                case array_index(ABILITIES, "Super Luck"):
-                case array_index(ABILITIES, "Compound Eyes"):
-                    item1Rate = ITEM1_BOOSTEDRATE;
-                    item2Rate = ITEM2_BOOSTEDRATE;
-                    break;
-                default:
-                    item1Rate = ITEM1_BASERATE;
-                    item2Rate = ITEM2_BASERATE;
-                    break;
-            }
-        }
 
         auto __this = (PokeParty::Object*) ctx->X[0];
         auto idx = (uint32_t) ctx->W[1];
@@ -54,7 +28,7 @@ HOOK_DEFINE_INLINE(SetWildPokemonItem) {
         PokePara::PokemonParam* enemyPoke = __this->GetMemberPointer(0);
         int32_t randomRoll = UnityEngine::Random::Range(0,100);
 
-        uint16_t setItem = Dpr::EncountTools::SetWildPokemonItem(enemyPoke, randomRoll, item1Rate, item2Rate);
+        uint16_t setItem = Dpr::EncountTools::SetWildPokemonItem(enemyPoke, randomRoll, 0, 0);
         Logger::log("[SetWildPokemonItem] Item set to %s\n", ITEMS[setItem]);
 
         ctx->X[0] = (uint64_t) __this->GetMemberPointer(idx);
