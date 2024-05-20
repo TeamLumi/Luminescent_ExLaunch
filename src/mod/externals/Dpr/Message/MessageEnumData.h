e886b9935fef67cbc496fb7dc7148f4eac1ee27f
Huevos <39507107+El-Huevo@users.noreply.github.com>
2024-05-20 11:38:21 +1000

    Update MessageEnumData.h
    - Added to Dpr::Message namespace

--- src/mod/externals/Dpr/Message/MessageEnumData.h
+++ src/mod/externals/Dpr/Message/MessageEnumData.h
@@ -2,17 +2,19 @@
 
 #include "externals/il2cpp-api.h"
 
-struct MessageEnumData : ILClass<MessageEnumData> {
-    enum class MsgLangId : int32_t {
-        JPN = 1,
-        USA = 2,
-        FRA = 3,
-        ITA = 4,
-        DEU = 5,
-        ESP = 7,
-        KOR = 8,
-        SCH = 9,
-        TCH = 10,
-        Num = 11,
+namespace Dpr::Message {
+    struct MessageEnumData : ILClass<MessageEnumData> {
+        enum class MsgLangId : int32_t {
+            JPN = 1,
+            USA = 2,
+            FRA = 3,
+            ITA = 4,
+            DEU = 5,
+            ESP = 7,
+            KOR = 8,
+            SCH = 9,
+            TCH = 10,
+            Num = 11,
+        };
     };
-};
+}
