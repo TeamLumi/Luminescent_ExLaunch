07d79be95e507f7e3c1abcb3da1769d314c00854
TwistedSpoon <39507107+VeryTwistedSpoon@users.noreply.github.com>
2024-09-04 18:44:44 +1000

    Utils cleanup
    - Removes redundant code and uses GetWorkOrIntValue
    - Implements a GetStringText() function into utils

--- src/mod/features/commands/case_call.cpp
+++ src/mod/features/commands/case_call.cpp
@@ -16,25 +16,9 @@ bool CaseCall(Dpr::EvScript::EvDataManager::Object* manager)
 
     if (args->max_length >= 3)
     {
-        auto argType = static_cast<EvData::ArgType>(args->m_Items[1].fields.argType);
 
-        if (argType == EvData::ArgType::Work) {
-            workNoValue = args->m_Items[1].fields.data;
-        }
-        else if (argType == EvData::ArgType::Float) {
-            workNoValue = static_cast<float>(args->m_Items[1].fields.data);
-        }
-        else {
-            workNoValue = 0;
-        }
-
-        EvData::Aregment::Object arg3 = args->m_Items[2];
-        System::String::Object* label = System::String::Create("");
-
-        if (static_cast<EvData::ArgType>(arg3.fields.argType) == EvData::ArgType::String) {
-            EvData::Object* evData = manager->fields._evData->fields._EvData;
-            label = evData->GetString(arg3.fields.data);
-        }
+        workNoValue = GetWorkOrIntValue(args->m_Items[1]);
+        System::String::Object* label = GetStringText(manager, args->m_Items[2]);
 
         if (switchWorkIndex == workNoValue) {
             manager->CallLabel(label);
