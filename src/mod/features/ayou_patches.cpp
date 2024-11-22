486d42b9474ee91a3bb8dd5f2dcf265ed81481e4
SaltContainer <fanmc34@gmail.com>
2024-11-21 20:39:20 -0500

    Fix AYou trainer name crash

--- src/mod/features/ayou_patches.cpp
+++ src/mod/features/ayou_patches.cpp
@@ -9,25 +9,26 @@
 
 using namespace Dpr::Message;
 
-HOOK_DEFINE_TRAMPOLINE(GetNameMessage) {
-    static System::String::Object* Callback(Dpr::Message::MessageManager::Object* __this, System::String::Object* fileName,
-                                            System::String::Object* label) {
-        bool isAYou = strcmp(label->asCString().c_str(), "DLP_SPEAKERS_NAME_AYOU") == 0;
+static const char* AYOU_SPEAKER_NAME_LABEL = "DLP_SPEAKERS_NAME_AYOU";
+static const char* AYOU_TRAINER_NAME_LABEL = "DP_TRAINERS_NAME_TR_AYOU";
+
+HOOK_DEFINE_TRAMPOLINE(MessageManager$$GetNameMessage) {
+    static System::String::Object* Callback(Dpr::Message::MessageManager::Object* __this, System::String::Object* fileName, System::String::Object* label) {
+        bool isAYou = strcmp(label->asCString().c_str(), AYOU_SPEAKER_NAME_LABEL) == 0;
 
         return isAYou ? System::String::Create(getCustomSaveData()->ayou.name) : Orig(__this, fileName, label);
     }
 };
 
-HOOK_DEFINE_INLINE(register_TrainerName) {
-    static void Callback(exl::hook::nx64::InlineCtx* ctx) {
-        auto labelName = reinterpret_cast<System::String::Object*>(ctx->X[1]);
-        bool isAYou = labelName->asCString().find("DP_TRAINERS_NAME_TR_AYOU") == 0;
-        isAYou ? MessageWordSetHelper::SetStringWord(0, System::String::Create(
-                getCustomSaveData()->ayou.name)) : MessageWordSetHelper::SetTrainerNameWord(0, labelName);
+HOOK_DEFINE_TRAMPOLINE(MessageWordSetHelper$$SetTrainerNameWord) {
+    static void Callback(int32_t tagIndex, System::String::Object* labelName) {
+        bool isAYou = strcmp(labelName->asCString().c_str(), AYOU_TRAINER_NAME_LABEL) == 0;
+
+        isAYou ? MessageWordSetHelper::SetStringWord(tagIndex, System::String::Create(getCustomSaveData()->ayou.name)) : Orig(tagIndex, labelName);
     }
 };
 
 void exl_ayou_patches_main() {
-    GetNameMessage::InstallAtOffset(0x0210ce50);
-    register_TrainerName::InstallAtOffset(0x018839ec);
+    MessageManager$$GetNameMessage::InstallAtOffset(0x0210ce50);
+    MessageWordSetHelper$$SetTrainerNameWord::InstallAtOffset(0x018839ec);
 }
