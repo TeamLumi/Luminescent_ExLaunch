a7f0f9c2c5f5e555a3220a9e966db62334e2e341
SaltContainer <fanmc34@gmail.com>
2025-02-15 13:11:55 -0500

    Change Rowan to use the coat model in intro

--- src/mod/features/intro_professor_pokemon.cpp
+++ src/mod/features/intro_professor_pokemon.cpp
@@ -6,7 +6,7 @@
 
 #include "utils/utils.h"
 
-HOOK_DEFINE_INLINE(Demo_Hakase_PreloadAssetBundles) {
+HOOK_DEFINE_INLINE(Mon_Demo_Hakase_PreloadAssetBundles) {
     static void Callback(exl::hook::nx64::InlineCtx* ctx) {
         auto profMon = GetProfessorMon();
         ctx->W[0] = profMon.monsno;
@@ -16,7 +16,7 @@ HOOK_DEFINE_INLINE(Demo_Hakase_PreloadAssetBundles) {
     }
 };
 
-HOOK_DEFINE_INLINE(Demo_Hakase_ReleasePreloadedAssetBundles) {
+HOOK_DEFINE_INLINE(Mon_Demo_Hakase_ReleasePreloadedAssetBundles) {
     static void Callback(exl::hook::nx64::InlineCtx* ctx) {
         auto profMon = GetProfessorMon();
         ctx->W[0] = profMon.monsno;
@@ -26,7 +26,7 @@ HOOK_DEFINE_INLINE(Demo_Hakase_ReleasePreloadedAssetBundles) {
     }
 };
 
-HOOK_DEFINE_INLINE(Demo_Hakase_Enter_d__15_MoveNext) {
+HOOK_DEFINE_INLINE(Mon_Demo_Hakase_Enter_d__15_MoveNext) {
     static void Callback(exl::hook::nx64::InlineCtx* ctx) {
         auto disp = (Dpr::Demo::DemoBase::LoadPokeAsset_d__71::Object*)ctx->X[22];
         auto profMon = GetProfessorMon();
@@ -38,7 +38,7 @@ HOOK_DEFINE_INLINE(Demo_Hakase_Enter_d__15_MoveNext) {
     }
 };
 
-HOOK_DEFINE_INLINE(Demo_Hakase_Main_b__16_7) {
+HOOK_DEFINE_INLINE(Mon_Demo_Hakase_Main_b__16_7) {
     static void Callback(exl::hook::nx64::InlineCtx* ctx) {
         auto profMon = GetProfessorMon();
         ctx->W[0] = profMon.monsno;
@@ -46,9 +46,38 @@ HOOK_DEFINE_INLINE(Demo_Hakase_Main_b__16_7) {
     }
 };
 
+HOOK_DEFINE_INLINE(Prof_Demo_Hakase_PreloadAssetBundles) {
+    static void Callback(exl::hook::nx64::InlineCtx* ctx) {
+        ctx->X[0] = (uint64_t)System::String::Create("persons/battle/tr2003_00");
+    }
+};
+
+HOOK_DEFINE_INLINE(Prof_Demo_Hakase_ReleasePreloadedAssetBundles) {
+    static void Callback(exl::hook::nx64::InlineCtx* ctx) {
+        ctx->X[0] = (uint64_t)System::String::Create("persons/battle/tr2003_00");
+    }
+};
+
+HOOK_DEFINE_INLINE(Prof_Demo_Hakase_Enter_d__15_MoveNext) {
+    static void Callback(exl::hook::nx64::InlineCtx* ctx) {
+        ctx->X[0] = (uint64_t)System::String::Create("persons/battle/tr2003_00");
+    }
+};
+
+HOOK_DEFINE_INLINE(Prof_Demo_Hakase_Enter_b__15_1) {
+    static void Callback(exl::hook::nx64::InlineCtx* ctx) {
+        ctx->X[1] = (uint64_t)System::String::Create("tr2003_00");
+    }
+};
+
 void exl_intro_professor_pokemon_main() {
-    Demo_Hakase_PreloadAssetBundles::InstallAtOffset(0x01ad2138);
-    Demo_Hakase_ReleasePreloadedAssetBundles::InstallAtOffset(0x01ad2350);
-    Demo_Hakase_Enter_d__15_MoveNext::InstallAtOffset(0x01ad4108);
-    Demo_Hakase_Main_b__16_7::InstallAtOffset(0x01ad3308);
+    Mon_Demo_Hakase_PreloadAssetBundles::InstallAtOffset(0x01ad2138);
+    Mon_Demo_Hakase_ReleasePreloadedAssetBundles::InstallAtOffset(0x01ad2350);
+    Mon_Demo_Hakase_Enter_d__15_MoveNext::InstallAtOffset(0x01ad4108);
+    Mon_Demo_Hakase_Main_b__16_7::InstallAtOffset(0x01ad3308);
+
+    Prof_Demo_Hakase_PreloadAssetBundles::InstallAtOffset(0x01ad2038);
+    Prof_Demo_Hakase_ReleasePreloadedAssetBundles::InstallAtOffset(0x01ad2280);
+    Prof_Demo_Hakase_Enter_d__15_MoveNext::InstallAtOffset(0x01ad3fb4);
+    Prof_Demo_Hakase_Enter_b__15_1::InstallAtOffset(0x01ad2d04);
 }
